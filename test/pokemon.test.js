78757e2c28ea7b451da9aea6215df5c0310fb928
Triana Cerda <trianaRcerda@gmail.com>
2021-07-13 17:58:07 -0700

    working tests

--- test/pokemon.test.js
+++ test/pokemon.test.js
@@ -4,13 +4,31 @@ import { showPokemon } from '../storage-utils.js';
 const test = QUnit.test;
 
 test('showPokemon should create a results object if pokemon not previously shown', (expect) => {
-
+    localStorage.removeItem('RESULTS');
     const fakeResults = {
         id: 1,
         shown: 1,
         preferred: 0
     };
 
+    showPokemon(1);
+
+    const resultsString = localStorage.getItem('RESULTS') || '[]';
+    const results = JSON.parse(resultsString);
+
+    expect.deepEqual(results[0], fakeResults);
+});
+
+test('showPokemon should increment by results if pokemon previously shown', (expect) => {
+    localStorage.removeItem('RESULTS');
+    const fakeResults = {
+        id: 1,
+        shown: 3,
+        preferred: 0
+    };
+
+    showPokemon(1);
+    showPokemon(1);
     showPokemon(1);
 
     const resultsString = localStorage.getItem('RESULTS') || '[]';
