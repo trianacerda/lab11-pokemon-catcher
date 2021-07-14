9f0d0837b8c4fa21fbd7143b1547012f7a7eced2
Triana Cerda <trianaRcerda@gmail.com>
2021-07-14 13:33:17 -0700

    changed file path to data/pokemon.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // import functions and grab DOM elements
-import data from '../data/pokemon.js';
+import data from './data/pokemon.js';
 
 const pokemon1 = document.getElementById('pokemon-1');
 const pokemon2 = document.getElementById('pokemon-2');
@@ -9,12 +9,9 @@ const image2 = document.getElementById('img-2');
 const image3 = document.getElementById('img-3');
 const button = document.getElementById('submit-btn');
 
-// initialize state
+
 let totalPlays = 0;
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+
 function renderRandomPokemon() {
     totalPlays++;
     let randomNum1 = Math.floor(Math.random() * data.length);
